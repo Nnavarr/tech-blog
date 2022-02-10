49d8e05d7cb350b5307e3c4f4bd40a8bfd554267
eon <noe.navarro1019@gmail.com>
2022-02-09 23:44:52 -0700

    created initial User database model

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+// create user model
+// TODO: create method for checking password
+class User extends Model {}
+
+// create colulmns for user model
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+)
+
+module.exports = User;
