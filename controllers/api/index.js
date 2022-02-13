8ffe0f2cbb6b7fc26fa0504120e591792af12bd4
eon <noe.navarro1019@gmail.com>
2022-02-13 12:41:44 -0700

    created session object

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 // individual file routes
 const commentRoutes = require('./comment-routes');
 const postRoutes = require('./post-routes');
+const userRoutes = require('./user-routes');
 
 // define the endpoint to use
 router.use('/comments', commentRoutes);
 router.use('/posts', postRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
---
+++ controllers/api/user-routes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+// get user by id
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+// add new user
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+})
+
+module.exports = router;
