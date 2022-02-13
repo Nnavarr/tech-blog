0b32bcec6d13387fc8971d187b7c1e375fa0034a
eon <noe.navarro1019@gmail.com>
2022-02-12 20:07:35 -0700

    formatted controller and api index folders. Created test comment GET route

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+// define express and params
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
