e3624235969795646367bded914b74558b68abbc
eon <noe.navarro1019@gmail.com>
2022-02-12 21:33:18 -0700

    crated post endpoints

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+// individual file routes
 const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
 
+// define the endpoint to use
 router.use('/comments', commentRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
---
+++ controllers/api/post-routes.js
@@ -0,0 +1,87 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Post, User, Comment } = require('../../models');
+
+// get all posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'post_text',
+            'user_id',
+            'date_created'
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+// get post by id
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_text',
+            'user_id',
+            'date_created'
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+// create new post
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        post_text: req.body.post_text,
+        // TODO: update to session user id
+        user_id: req.body.user_id,
+        date_created: req.body.date_created
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+// update posts
+router.put('/:id', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id'});
+            return;
+        }
+        res.json(dbPostData)
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+module.exports = router;
