0ca1a532453b2b38cf104d444270d44f01264fb8
eon <noe.navarro1019@gmail.com>
2022-02-12 20:30:44 -0700

    added various api endpoints for comments including POST and DELETE

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
+// get all comments
 router.get('/', (req, res) => {
     Comment.findAll()
       .then(dbCommentData => res.json(dbCommentData))
@@ -10,4 +11,64 @@ router.get('/', (req, res) => {
       });
   });
 
-  module.exports = router;
+// get comment by id
+router.get('/:id', (req, res) => {
+    Comment.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'comment_text',
+            'user_id',
+            'post_id',
+            'date_created'
+        ]
+    })
+    .then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with this id'});
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+// post comment
+router.post('/', (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id,
+        date_created: req.body.date_created
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        res.status(400).json(err);
+    })
+})
+
+// delete comment
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(400).json({ message: 'No comment found with this id'});
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+
+module.exports = router;
