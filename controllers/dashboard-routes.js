470ac406f972cf38797c901ae9ffe1d9d7540e29
eon <noe.navarro1019@gmail.com>
2022-02-13 16:24:10 -0700

    added dashborad view once the user is logged in

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,3 +2,27 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
+
+// get all posts 
+router.get('/', withAuth, (req,res) => {
+    // get all posts
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'post_text',
+            'user_id',
+            'date_created'
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+
+        res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+module.exports = router;
