57f880280698047fa2bdf4b46f7569ee990b53ab
eon <noe.navarro1019@gmail.com>
2022-02-13 13:07:50 -0700

    created login, logout, and delete routes for user endpoints

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -39,6 +39,82 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
+    .then(dbUserData => {
+        // save session
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        })
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+
+// user login
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address! '});
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        // save session
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        })
+    })
+})
+
+// user logout 
+router.post('/logout', (req, res) => {
+    if (req.sessions.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
+// delete user
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
 })
 
 module.exports = router;
