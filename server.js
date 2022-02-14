0596f7723c446feb0dc3bae4994c271f0a894ef9
eon <noe.navarro1019@gmail.com>
2022-02-13 21:33:47 -0700

    added helper functionality

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 
 // define express and params
 const app = express();
@@ -22,6 +21,10 @@ const sess = {
 };
 
 app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
